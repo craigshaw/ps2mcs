9a315ece6da3ae2e7e0a37f96ce888cc17d951cb
Craig Shaw <craig@craigshaw.com>
2024-10-02 21:40:42 +0100

    Add async FTP

--- ps2mcs.py
+++ ps2mcs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+import aioftp
 import argparse
+import asyncio
 import json
 import os
 import re
 import time
 
 from datetime import datetime
-from ftplib import FTP
 from pathlib import Path
 
 from mapping.flat import FlatMappingStrategy
@@ -37,21 +38,11 @@ def main():
         # Get file mapping strategy
         ms = create_mapping_strategy()
 
-        vmcs_to_sync = get_vmcs_to_sync()
+        vmcs_to_sync = get_vmcs_to_sync(ms, local_root)
         
-        ftp = create_ftp_connection(args.ftp_host, uname, pwd)
-
         start_time = time.perf_counter()
 
-        for i in range(len(vmcs_to_sync)):
-            vmc_path = vmcs_to_sync[i]
-            local_path = local_root / Path(ms.map_remote_to_local(vmc_path))
-            print(f'[{i+1}/{len(vmcs_to_sync)}]> {vmc_path} <---> {local_path}')
-
-            # Check path to file locally exists, create if not
-            local_path.parent.mkdir(parents=True, exist_ok=True)
-        
-            sync_file(ftp, local_path, vmc_path)
+        asyncio.run(sync_targets(args.ftp_host, uname, pwd, vmcs_to_sync))
 
         sync_time = (time.perf_counter() - start_time) * 1000
 
@@ -59,7 +50,7 @@ def main():
     except Exception as e:
         print(f'Failed to sync: {e}')
 
-def get_vmcs_to_sync():
+def get_vmcs_to_sync(ms, local_root):
     with open(SYNC_TARGETS, 'r') as f:
         config = json.load(f)
 
@@ -68,16 +59,28 @@ def get_vmcs_to_sync():
     vmc_paths = []
 
     for vmc in vmcs_to_sync:
+        # Get the target path
         target_path = Path(MCPD2_PS2_ROOT) / Path(vmc)
         validate_remote_path(target_path)
-        vmc_paths.append(target_path)
+
+        # Map to a local path
+        local_path = local_root / Path(ms.map_remote_to_local(target_path))
+        # Check path to file locally exists, create if not
+        local_path.parent.mkdir(parents=True, exist_ok=True)
+
+        vmc_paths.append((target_path, local_path))
 
     return vmc_paths
 
-def sync_file(ftp, local_path, remote_path):
+async def sync_targets(ftp_host, user, pwd, targets):
+    async with aioftp.Client.context(ftp_host, user=user, password=pwd) as client:
+        for (lp, rp) in targets:
+            await sync_file(client, rp, lp)
+
+async def sync_file(ftp, local_path, remote_path):
     try:
         # Get remote file's modified time
-        rmt = get_remote_modified_time(ftp, remote_path)
+        rmt = await get_remote_modified_time(ftp, remote_path)
 
         # Check if local file exists
         if os.path.exists(local_path):
@@ -86,33 +89,36 @@ def sync_file(ftp, local_path, remote_path):
             # Compare times and decide action
             if rmt > lmt:
                 print(f'Remote file is newer. Downloading {local_path}')
-                sync_remote_file_to_local(ftp, remote_path, local_path, rmt)
+                await sync_remote_file_to_local(ftp, remote_path, local_path, rmt)
             elif lmt > rmt:
                 print(f'Local file is newer. Uploading {remote_path}')
-                sync_local_file_to_remote(ftp, local_path, remote_path)
+                await sync_local_file_to_remote(ftp, local_path, remote_path)
             else:
                 print(f'Files are in sync')
         else:
             print(f'Local file doesn\'t exist. Downloading {local_path}')
-            sync_remote_file_to_local(ftp, remote_path, local_path, rmt)
+            await sync_remote_file_to_local(ftp, remote_path, local_path, rmt)
     except Exception as e:
         print(f'Error syncing file {remote_path}: {e}')
 
-def sync_local_file_to_remote(ftp, local_path, remote_path):
-    with open(local_path, 'rb') as f:
-        ftp.storbinary(f'STOR {remote_path}', f)
+async def sync_local_file_to_remote(ftp, local_path, remote_path):
+    await ftp.upload(local_path, remote_path, write_into=True)
 
     # Because we can't update the modified time on the MCP2 (no MFMT command support)...
     # lets update the timestamp on our local file to that of the time on the MCP2
-    rmt = get_remote_modified_time(ftp, remote_path)
+    rmt = await get_remote_modified_time(ftp, remote_path)
     os.utime(local_path, (rmt, rmt))
 
-def sync_remote_file_to_local(ftp, remote_path, local_path, rmt):
-    with open(local_path, 'wb') as f:
-        ftp.retrbinary(f'RETR {remote_path}', f.write)
+async def sync_remote_file_to_local(ftp, remote_path, local_path, rmt):
+    await ftp.download(remote_path, local_path, write_into=True)
 
     # Update local timestamp so it reflects the remote time
     os.utime(local_path, (rmt, rmt))
+
+async def get_remote_modified_time(ftp, filename):
+    response = await ftp.command(f"MDTM {filename}", "213") # '213 YYYYMMDDHHMMSS'
+    remote_time_str = response[1][0].strip()
+    return ftp_time_to_unix_timestamp(remote_time_str)
 
 def validate_remote_path(target_path):
     pattern = fr'^{MCPD2_PS2_ROOT}/([^/]+)/([^/]+)-([1-8])\.mc2$'
@@ -122,18 +128,8 @@ def validate_remote_path(target_path):
     if match is None:
         raise InvalidTargetFormatError()
 
-def get_remote_modified_time(ftp, filename):
-    response = ftp.sendcmd(f"MDTM {filename}") # '213 YYYYMMDDHHMMSS'
-    remote_time_str = response[4:]
-    return ftp_time_to_unix_timestamp(remote_time_str)
-
 def ftp_time_to_unix_timestamp(ftp_time_str):
     return int(datetime.strptime(ftp_time_str, '%Y%m%d%H%M%S').timestamp())
-
-def create_ftp_connection(ftp_host, uname, pwd):
-    ftp = FTP(ftp_host)
-    ftp.login(uname, pwd)
-    return ftp
 
 def create_mapping_strategy():
     return FlatMappingStrategy()
